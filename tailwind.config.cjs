ad1e62fb97bbd84f257e0782f758e608cd9adfc9
David Beechey <david@dtbeechey.dev>
2023-06-20 20:02:33 +0100

    Set up Prettier config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,15 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       colors: {
         primary: colors.sky,
         secondary: colors.rose,
       },
     },
-	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
