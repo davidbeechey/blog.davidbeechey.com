85ddcd43138e100b05820fbdf0681ac109a3151a
David Beechey <david@dtbeechey.dev>
2023-06-20 19:31:53 +0100

    Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        primary: colors.sky,
+        secondary: colors.rose,
+      },
+    },
 	},
 	plugins: [],
 }
