07bf660d2df9da00656def1b2efe968d8e57a706
David Beechey <david@dtbeechey.dev>
2023-06-23 20:11:12 +0100

    Add MDX support

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()],
-  site: "http://localhost:3000",
+  integrations: [tailwind(), sitemap(), react(), mdx()],
+  site: "http://localhost:3000"
 });
