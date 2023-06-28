e66ba2ee3b1a1ab7799dfe1f9ede8297524a3e6f
David Beechey <david@dtbeechey.dev>
2023-06-28 12:24:54 +0100

    Fix tags for draft posts being displayed

--- src/tags.ts
+++ src/tags.ts
@@ -1,10 +1,12 @@
 import { getCollection } from "astro:content";
 
-const blogs = await getCollection("blog");
+let posts = await getCollection("blog");
+
+import.meta.env.PROD && (posts = posts.filter((post) => !post.data.draft));
 
 // Get all of the tags and how many times they are used
-export const tags = blogs.reduce((acc, blog) => {
-  blog.data.tags?.forEach((tag) => {
+export const tags = posts.reduce((acc, post) => {
+  post.data.tags?.forEach((tag) => {
     acc[tag] ? acc[tag]++ : (acc[tag] = 1);
   });
   return acc;
